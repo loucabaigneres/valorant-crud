d0fcc7c2b773587ac2b0f2b0ab583569c891d02f
Louca <baigneres.louca@gmail.com>
2024-12-18 23:41:00 +0100

    ✨ Add /agents endpoint to return a list of agents and update package.json with start and dev scripts

--- back/index.js
+++ back/index.js
@@ -11,6 +11,36 @@ app.get("/", (req, res) => {
   res.send("Hello, world!")
 })
 
+app.get("/agents", (req, res) => {
+  res.json([
+    "Brimstone",
+    "Phoenix",
+    "Sage",
+    "Sova",
+    "Viper",
+    "Cypher",
+    "Reyna",
+    "Killjoy",
+    "Breach",
+    "Omen",
+    "Jett",
+    "Raze",
+    "Skye",
+    "Yoru",
+    "Astra",
+    "KAY/O",
+    "Chamber",
+    "Neon",
+    "Fade",
+    "Harbor",
+    "Gekko",
+    "Deadlock",
+    "Iso",
+    "Clove",
+    "Vyse",
+  ])
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
 })
